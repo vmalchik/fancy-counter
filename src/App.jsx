ea5d4e0d950ea0d118618263b57f935adb1b534a
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-10 23:56:04 -0700

    create components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// Benefits of components
+// 1. Reusability
+// 2. Maintainability
+// 3. Testability
+// 4. Readability
+// 5. Consistency
+// 6. Productivity
+// 7. Collaboration
+
+import Card from "./Card";
+
+function App() {
+  return (
+    <main>
+      <Card />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/CountButtons.jsx
@@ -0,0 +1,8 @@
+export default function CountButtons() {
+  return (
+    <div>
+      <button>-</button>
+      <button>+</button>
+    </div>
+  );
+}
