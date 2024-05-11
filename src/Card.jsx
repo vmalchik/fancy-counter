4416cd73ac3bde4326684fec6eb542cad59d8ecc
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-11 11:23:31 -0700

    show count state

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import Count from "./Count";
-import CountButtons from "./CountButtons";
+import ButtonsContainer from "./ButtonContainer";
 import ResetButton from "./ResetButton";
 import Title from "./Title";
 
 export default function Card() {
+  // When state changes (e.g. count is updated) then all child components of Card
+  // will re-render unless they are optimized with React.memo or useMemo
+  const [count, setCount] = useState(0);
   return (
     <div className="card">
       <Title />
-      <Count />
-      <ResetButton />
-      <CountButtons />
+      <Count count={count} />
+      <ResetButton setCount={setCount} />
+      <ButtonsContainer setCount={setCount} />
     </div>
   );
 }
--- src/Count.jsx
+++ src/Count.jsx
@@ -1,3 +1,3 @@
-export default function Count() {
-  return <p className="count">6</p>;
+export default function Count({ count }) {
+  return <p className="count">{count}</p>;
 }
